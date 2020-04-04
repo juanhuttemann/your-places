95c8c4ddd7efc1bea170cabd3ed0218df01dd9e1
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-04 14:17:22 -0400

    add custom buttons

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
 import "./PlaceItem.css";
 
 const PlaceItem = props => {
-  const {image, description, address, title, creatorId, coordinates} = props
+  const {id, image, description, address, title, creatorId, coordinates} = props
   return (
     <li className="place-item">
       <Card className="place-item__content">
@@ -17,9 +18,9 @@ const PlaceItem = props => {
           <p>{description}</p>
         </div>
         <div className="place-item__actions">
-          <button>View on Map</button>
-          <button>Edit</button>
-          <button>Delete</button>
+          <Button inverse>View on Map</Button>
+          <Button to={`/places/${id}`}>Edit</Button>
+          <Button danger>Delete</Button>
         </div>
       </Card>
     </li>
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import PlaceList from '../components/PlaceList'
 
 const DUMMY_PLACES = [
@@ -31,7 +32,9 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-  return <PlaceList places={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+  return <PlaceList places={loadedPlaces} />;
 }
 
 export default UserPlaces
