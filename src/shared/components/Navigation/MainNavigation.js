556585346f1641e7609102911a8f05b898ff33ca
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-04 13:20:53 -0400

    add SideDrawer animation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer'
+import Backdrop from '../UIElements/Backdrop'
 import './MainNavigation.css'
 
 const MainNavigation = props =>{
-  return <MainHeader>
-    <button className="main-navigation__menu-btn">
-      <span/>
-      <span/>
-      <span/>
-    </button>
-    <h1 className="main-navigation__title">
-      Your Places
-    </h1>
-    <nav>
-      ...
-    </nav>
-  </MainHeader>
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+
+  const openDrawerHandler = ()=>{
+    setDrawerIsOpen(true)
+  }
+
+  const closeDrawerHandler = ()=>{
+    setDrawerIsOpen(false)
+  }
+
+  return (
+    <React.Fragment>
+      { drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer 
+        show={drawerIsOpen}
+        onClick={closeDrawerHandler}
+      >
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button
+          onClick={openDrawerHandler} 
+          className="main-navigation__menu-btn"
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">Your Places</h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
 }
 
 export default MainNavigation
